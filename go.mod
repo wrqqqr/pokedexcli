e602b655c8659a44ab8302324e34afaa8c6b7532
wrqqqr <wrqqqrrr@rambler.ru>
2024-08-25 01:43:29 +0300

    start

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wrqqqr/pokedexcli
+
+go 1.23.0
---
+++ repl.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(" >")
+
+		scanner.Scan()
+		text := scanner.Text()
+
+		fmt.Println("echoing: ", text)
+	}
+
+}
+
+func clearInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+
+	return words
+}
